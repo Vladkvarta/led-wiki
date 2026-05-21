2827548792f1f8a841ef755d0026c9de08f665cf
Vladkvarta <Vladkvarta@gmail.com>
2026-05-21 23:23:35 +0300

    v2.1

--- js/modules/state.js
+++
@@ -1,7 +0,0 @@
-export const state = {
-  sortKey: 'price',
-  sortDir: 1,
-  visibleGroups: new Set(['core','performance','software','commercial']),
-  activeFilters: {hdr:false, genlock:false, cloud:false, latency:false},
-  activePreset: null,
-};
